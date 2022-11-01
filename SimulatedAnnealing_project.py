9e7572d55fd044d3b33157494a2986b67920503b
tiagosilveriom <tiago.ars.machado@gmail.com>
2022-11-01 09:48:44 +0100

    Simullated Annealing updated

--- SimulatedAnnealing_project.py
+++ SimulatedAnnealing_project.py
@@ -2,11 +2,14 @@ import time
 import numpy as np
 
 class simullated_annealing_parameters:
-    def __init__(self,temperature,solution,cost,cooling_factor):
+    def __init__(self,temperature,solution,cost,cooling_factor,number_poll_servers,budget_poll_servers,period_poll_servers):
         self.curr_temp=temperature
         self.cool=cooling_factor
         self.best_solution=solution
         self.best_cost=cost
+        self.number_poll_servers=number_poll_servers
+        self.budget_poll_servers=budget_poll_servers
+        self.period_poll_servers=period_poll_servers
 
 def cost_function(TT_tasks_WCRT,ET_tasks_WCRT,ET_schedule):
 
@@ -37,7 +40,8 @@ def SimulatedAnnealing(TT_tasks_WCRT,ET_tasks_WCRT,ET_schedule,candidate_solutio
         parameters.best_solution = candidate_solution
         # if the solution is worse there is a random but decreasing chance to accept it
     else:
-        if np.random.rand() < np.exp(-(candidate_cost - parameters.best_cost)/parameters.curr_temp):
+        #Lower current temperature will decrease the chance of accepting worse solution
+        if np.random.rand() < np.exp((candidate_cost - parameters.best_cost)/parameters.curr_temp):
             parameters.best_cost = candidate_cost
             parameters.best_solution=candidate_solution
 
@@ -45,16 +49,25 @@ def SimulatedAnnealing(TT_tasks_WCRT,ET_tasks_WCRT,ET_schedule,candidate_solutio
     parameters.curr_temp = parameters.cool*parameters.curr_temp
 
     # return the new random changes to have the next candidates
+
     # we are still going to discuss about the boundaries
     
-    max_number_poll_servers=4
-    number_poll_servers=np.random.randint(1,max_number_poll_servers,1)
-    max_budget=300
-    min_budget=50
-    budget_poll_servers=np.random.randint(min_budget,max_budget,size=1)
-    min_period=100
-    max_period=600
-    period_poll_servers=np.random.randint(min_period,max_period,size=1)
+    max_number_poll_servers_variation=2
+    number_poll_servers__variation=np.random.randint(-max_number_poll_servers_variation,max_number_poll_servers_variation,1)
+    max_budget_variation=300
+    budget_poll_servers_variation=np.random.randint(-max_budget_variation,max_budget_variation,size=1)
+    max_period_variation=300
+    period_poll_servers_variation=np.random.randint(max_period_variation,max_period_variation,size=1)
+
+    #number_poll_servers=max(1,parameters.number_poll_servers+number_poll_servers__variation)
+    budget_poll_servers=max(100,budget_poll_servers+budget_poll_servers_variation)
+    #period_poll_servers=max(100,parameters.period_poll_servers+period_poll_servers_variation)
+
+    period_poll_servers=100
+    number_poll_servers=1
+    parameters.number_poll_servers=number_poll_servers
+    parameters.budget_poll_servers=budget_poll_servers
+    parameters.period_poll_servers=period_poll_servers
 
     return number_poll_servers,budget_poll_servers,period_poll_servers
 
