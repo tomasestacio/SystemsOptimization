f5976f2c425e3ec5d7787302b89f4463318d0e83
Tomás Estácio <97453864+tomasestacio@users.noreply.github.com>
2022-11-22 10:06:14 +0100

    Update extension2.py
    Extension 2 now works on reassigning priorities of ET tasks based on their durations.

--- extension2.py
+++ extension2.py
@@ -8,24 +8,26 @@ import numpy as np
 import math
 
 def ET_ext2(et_tasks):
-    # reassignement of the priorities of the ET tasks according to the EDF method -> earliest deadline, biggest priority
-    # 1) check the biggest deadline of the ET tasks in the test case
+    
+    # reassignement of the priorities of the ET tasks according to lowest duration, biggest priority
+    # 1) check the lowest duration of the ET tasks in the test case
     # 2) divide that by the number of ET tasks in the test case
     # 3) assign priorities based on the space division 
 
-    max_deadline = 0
+    big_ct = 0
 
     for task in et_tasks:
-        if(max_deadline < task.deadline):
-            max_deadline = task.deadline
+        if(big_ct < task.duration):
+            big_ct = task.duration
 
-    chunksize = math.floor(max_deadline/7)
+    chunksize = math.floor(big_ct/7)
+    if(big_ct % 7 > 0):
+        chunksize += 1
 
     for task in et_tasks:
         for i in range(0, 7):
-            if(task.deadline - (i+1)*chunksize < 0):
+            if(task.duration <= chunksize*(i+1)):
                 task.priority = i
                 break
 
     return et_tasks
-
